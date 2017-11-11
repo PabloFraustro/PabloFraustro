607a7fa9f9302d0428d3ddb62c8ac3a026c98b8b
Pablo Fraustro <pablofraustro@gmail.com>
2017-11-10 21:36:45 -0600

    Added all Dummy modals

--- js/animations.js
+++ js/animations.js
@@ -1,12 +1,33 @@
 $(document).ready(function() {
 
+//Slack effects
   $(".block_slack").on("click",function(){
     $("#Slackline_modal").modal('toggle');
   });
+
+  //Computer effects
 
   $(".block_computer").hover(function(){
     $("#Computer_ON").toggle();
     $(".block_computer").css("cursor","pointer");
   });
+
+  $(".block_computer").on("click",function(){
+    $("#Computer_modal").modal('toggle');
+  });
+
+//Woodman effects
+
+  $(".block_woodman").on("click",function(){
+    $("#Creativity_modal").modal('toggle');
+  });
+
+  //Electronics effects
+
+  $(".block_micro").on("click",function(){
+    $("#Electronics_modal").modal('toggle');
+  });
+
+
 
 });
