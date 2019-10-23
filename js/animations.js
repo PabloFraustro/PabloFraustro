a356b53a34e14c95de91c09bb86ae45c3a73e35b
Pablo Fraustro <pablofraustro@MBP-de-Pablo.mshome.net>
2019-10-23 23:10:13 +0200

    I've added animations with the bodymovin plugin ! Directly from after effects, note that an internal server nededs to be simulated.
    Extra: Removed the shitty "shaking" animations

--- js/animations.js
+++ js/animations.js
@@ -1,61 +1,78 @@
 
 $(document).ready(function() {
 
-//Slack effects
-  $(".block_slack").on("click",function(){
-    $("#Slackline_modal").modal('toggle');
-  });
+// //Slack effects
+//   $(".block_slack").on("click",function(){
+//     $("#Slackline_modal").modal('toggle');
+//   });
 
-  //Computer effects
+//   //Computer effects
 
-  $(".block_computer").hover(function(){
-    $("#Computer_ON").toggle();
-    $(".block_computer").css("cursor","pointer");
-  }
-);
+//   $(".block_computer").hover(function(){
+//     $("#Computer_ON").toggle();
+//     $(".block_computer").css("cursor","pointer");
+//   }
+// );
 
-  $(".block_computer").on("click",function(){
-    $("#Computer_modal").modal('toggle');
-  });
+//   $(".block_computer").on("click",function(){
+//     $("#Computer_modal").modal('toggle');
+//   });
 
-//Woodman effects
+// //Woodman effects
 
-  $(".block_woodman").on("click",function(){
-    $("#Creativity_modal").modal('toggle');
-  });
+//   $(".block_woodman").on("click",function(){
+//     $("#Creativity_modal").modal('toggle');
+//   });
 
-  //Electronics effects
+//   //Electronics effects
 
-  $(".block_micro").on("click",function(){
-    $("#Electronics_modal").modal('toggle');
-  });
+//   $(".block_micro").on("click",function(){
+//     $("#Electronics_modal").modal('toggle');
+//   });
 
-  //scroll top on computer modal
+//   //scroll top on computer modal
 
-  $('#Computer_modal').on('shown.bs.modal', function () {
-    $('#Computer_modal').scrollTop(0);
+//   $('#Computer_modal').on('shown.bs.modal', function () {
+//     $('#Computer_modal').scrollTop(0);
+// });
+
+// $('#Slackline_modal').on('shown.bs.modal', function () {
+//   $('#Slackline_modal').scrollTop(0);
+// });
+
+//   // Automatically close vimeo video when closing modal
+//   var $frame_1 = $('iframe#embedded_video_1');
+//   var $frame_2 = $('iframe#embedded_video_2');
+
+//   $('#Slackline_modal').on('hidden.bs.modal', function () {
+//     // saves the current iframe source
+//     var vidsrc_1 = $frame_1.attr('src');
+//     var vidsrc_2 = $frame_2.attr('src');
+
+//     // sets the source to nothing, stopping the video
+//     $frame_1.attr('src','');
+//     $frame_2.attr('src','');
+
+//     // sets it back to the correct link so that it reloads immediately on the next window open
+//     $frame_1.attr('src', vidsrc_1);
+//     $frame_2.attr('src', vidsrc_2);
+// });
+
+var anim_b2_1;
+
+$('.b2_1').mouseenter(function(){
+  $(".b2_1").css("cursor","pointer");
+  anim_b2_1 = bodymovin.loadAnimation({
+    container: document.getElementById('anim_b2_1'),
+    renderer: 'svg',
+    loop: true,
+    autoplay:true,
+    path: 'animation_test.json'
+  })
 });
 
-$('#Slackline_modal').on('shown.bs.modal', function () {
-  $('#Slackline_modal').scrollTop(0);
-});
-
-  // Automatically close vimeo video when closing modal
-  var $frame_1 = $('iframe#embedded_video_1');
-  var $frame_2 = $('iframe#embedded_video_2');
-
-  $('#Slackline_modal').on('hidden.bs.modal', function () {
-    // saves the current iframe source
-    var vidsrc_1 = $frame_1.attr('src');
-    var vidsrc_2 = $frame_2.attr('src');
-
-    // sets the source to nothing, stopping the video
-    $frame_1.attr('src','');
-    $frame_2.attr('src','');
-
-    // sets it back to the correct link so that it reloads immediately on the next window open
-    $frame_1.attr('src', vidsrc_1);
-    $frame_2.attr('src', vidsrc_2);
+$('.b2_1').mouseleave(function(){
+  anim_b2_1.destroy();
 });
 
 
