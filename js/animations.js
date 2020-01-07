4e4b2bf26482cbd5f48f7be2a4edecbbe096bcf9
Pablo <pablofraustro@gmail.com>
2020-01-07 09:42:09 -0600

    Fixed instragram media content, re-did the list section also for mobile( not to scroll all the content)

--- js/animations.js
+++ js/animations.js
@@ -107,87 +107,37 @@ $(document).ready(function() {
     $("#b2_4_modal").modal('toggle');
   });
 
-// $(".zoom").hover(function(){
-
-// $(this).addClass('transition');
-// }, function(){
-    
-// $(this).removeClass('transition');
-// });
-//   //Computer effects
-
-//   $(".block_computer").hover(function(){
-//     $("#Computer_ON").toggle();
-//     $(".block_computer").css("cursor","pointer");
-//   }
-// );
-
-//   $(".block_computer").on("click",function(){
-//     $("#Computer_modal").modal('toggle');
-//   });
-
-// //Woodman effects
-
-//   $(".block_woodman").on("click",function(){
-//     $("#Creativity_modal").modal('toggle');
-//   });
-
-//   //Electronics effects
-
-//   $(".block_micro").on("click",function(){
-//     $("#Electronics_modal").modal('toggle');
-//   });
-
-//   //scroll top on computer modal
-
-//   $('#Computer_modal').on('shown.bs.modal', function () {
-//     $('#Computer_modal').scrollTop(0);
-// });
-
-// $('#Slackline_modal').on('shown.bs.modal', function () {
-//   $('#Slackline_modal').scrollTop(0);
-// });
-
   // Automatically close vimeo video when closing modal
   var $frame_1 = $('#embedded_video_1');
   var $frame_2 = $('#embedded_video_2');
   var $frame_3 = $('.instagram-media');
+  var $frame_4 = $('.instagram-media-2');
 
 
 
   $('#b2_1_modal').on('hidden.bs.modal', function () {
     // saves the current iframe source
     var vidsrc_1 = $frame_1.attr('src');
+    var vidsrc_2 = $frame_2.attr('src');
+    var vidsrc_3 = $frame_3.attr('src');
+    var vidsrc_4 = $frame_3.attr('src');
+
 
     // sets the source to nothing, stopping the video
     $frame_1.attr('src','');
+    $frame_2.attr('src','');
+    $frame_3.attr('src','');
+    $frame_4.attr('src','');
 
     // sets it back to the correct link so that it reloads immediately on the next window open
     $frame_1.attr('src', vidsrc_1);
-});
-
-$('#b2_2_modal').on('hidden.bs.modal', function () {
-  // saves the current iframe source
-  var vidsrc_2 = $frame_2.attr('src');
-
-  // sets the source to nothing, stopping the video
-  $frame_2.attr('src','');
-
-  // sets it back to the correct link so that it reloads immediately on the next window open
-  $frame_2.attr('src', vidsrc_2);
+    $frame_2.attr('src', vidsrc_2);
+    $frame_3.attr('src', vidsrc_3);
+    $frame_4.attr('src', vidsrc_4);
 });
 
 
-$('#b2_1_modal').on('hidden.bs.modal', function () {
-  // saves the current iframe source
-  var vidsrc_3 = $frame_3.attr('src');
 
-  // sets the source to nothing, stopping the video
-  $frame_3.attr('src','');
-
-  // sets it back to the correct link so that it reloads immediately on the next window open
-  $frame_3.attr('src', vidsrc_3);
-});
 
 
 var anim_b2_1;
@@ -266,17 +216,16 @@ $('.b2_4').mouseleave(function(){
 
   $('.block_woodman').on("click",function(){
     $("#b2_1_modal").modal('toggle');
-    $('#midi_section').show();
-    $('#kuniyoshi_section').show();
-    $('#hand_section').show();
-    $('#marella_section').show();
-    $('#atm_section').show();
+    $('#midi_section').hide();
+    $('#kuniyoshi_section').hide();
+    $('#hand_section').hide();
+    $('#marella_section').hide();
+    $('#atm_section').hide();
   });
 
 
   $('.b1_mobile').on("click",function(){
     $("#b2_2_modal").modal('toggle');
-
   });
 
 
@@ -288,76 +237,5 @@ $('.b2_4').mouseleave(function(){
   $('.b3_mobile').on("click",function(){
     $("#b2_4_modal").modal('toggle');
   });
-  
-
-  //backup
-
-  // $('.block_woodman').on("click",function(){
-  //   anim_b2_1 = bodymovin.loadAnimation({
-  //     container: document.getElementById('anim_b2_1_mobile'),
-  //     renderer: 'svg',
-  //     loop: false,
-  //     autoplay:true,
-  //     path: 'js/animation_1/data.json'
-  //   })
-  //   anim_b2_1.setSpeed(4);
-  //   anim_b2_1.addEventListener("complete", function(){
-  //   $("#b2_1_modal").modal('toggle');
-  //   anim_b2_1.destroy();
-  //   });
-  //   $('#midi_section').show();
-  //   $('#kuniyoshi_section').show();
-  //   $('#hand_section').show();
-  // });
-
-
-  // $('.b1_mobile').on("click",function(){
-  //   anim_b2_2 = bodymovin.loadAnimation({
-  //     container: document.getElementById('anim_b2_2_mobile'),
-  //     renderer: 'svg',
-  //     loop: false,
-  //     autoplay:true,
-  //     path: 'js/animation_2/data_2.json'
-  //   })
-  //   anim_b2_2.setSpeed(4);
-  //   anim_b2_2.addEventListener("complete", function(){
-  //   $("#b2_2_modal").modal('toggle');
-  //   anim_b2_2.destroy();
-  //   });
-  // });
-
-
-  // $('.b2_mobile').on("click",function(){
-  //   anim_b2_3 = bodymovin.loadAnimation({
-  //     container: document.getElementById('anim_b2_3_mobile'),
-  //     renderer: 'svg',
-  //     loop: false,
-  //     autoplay:true,
-  //     path: 'js/animation_3/data_3.json'
-  //   })
-  //   anim_b2_3.setSpeed(4);
-  //   anim_b2_3.addEventListener("complete", function(){
-  //   $("#b2_3_modal").modal('toggle');
-  //   anim_b2_3.destroy();
-  //   });
-  // });
-
-
-  // $('.b3_mobile').on("click",function(){
-  //   anim_b2_4 = bodymovin.loadAnimation({
-  //     container: document.getElementById('anim_b2_4_mobile'),
-  //     renderer: 'svg',
-  //     loop: false,
-  //     autoplay:true,
-  //     path: 'js/animation_4/data_4.json'
-  //   })
-  //   anim_b2_4.setSpeed(4);
-  //   anim_b2_4.addEventListener("complete", function(){
-  //   $("#b2_4_modal").modal('toggle');
-  //   anim_b2_4.destroy();
-  //   });
-  // });
-  
-
 
 });
